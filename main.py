b0c61115650bfc3be842c7f39d526822d9748659
tflatischler <t.flatischler@icloud.com>
2025-08-28 09:05:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from controller import joyConController
+from joycontrol.controller import JoyConController
 
-jc_left = JoyConController("jc_left")
-jc_right = JoyConController("jc_right")
-jc_extra = JoyConController("jc_extra")
+
+jc_left = JoyConController('jc_left')
+jc_right = JoyConController('jc_right')
+jc_extra = JoyConController('jc_extra')
+
+
+while True:
+   
+    jc_left.update_inputs(jc_left.get_raw_buttons())
+    jc_right.update_inputs(jc_right.get_raw_buttons())
+    jc_extra.update_inputs(jc_extra.get_raw_buttons())
